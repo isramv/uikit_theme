6064e94921e3e231948b46d0797faecea62fe0ea
Israel Morales <israel@chapterthree.com>
2017-02-17 12:03:21 -0800

    improved theme

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Gulp file to compile the SASS files into CSS.
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
